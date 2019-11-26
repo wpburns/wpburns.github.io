abf34a18fcf9a270cc079608d75ca65e2c9395ec
wp.burns <wp.burns@ulster.ac.uk>
2019-11-26 01:04:05 +0000

    Initial Commit

---
+++ card_view_script.js
@@ -0,0 +1,105 @@
+var myHilitor = new Hilitor(".cards-table"); // id of the element to parse
+
+$(function() {
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $(".cards-table > tbody > tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+            myHilitor.apply(value);
+        });
+    });
+});
+
+$.ajax({
+    type: "GET",
+    url: "../test_metadata.csv",
+    success: function (data) {
+        $('.cards-table').append(tableHeaders(Papa.parse(data).data));
+        $('.cards-table').append(tableContents(Papa.parse(data).data));
+    }
+});
+
+function tableContents(tableData) {
+    var table = $('<tbody></tbody>');
+
+    $(tableData).slice(1).each(function (i, rowData) {
+        var row = $('<tr></tr>');
+
+        $(rowData).each(function (j, cellData) {
+            if(cellData.includes('jpg')){
+                row.append($('<td>'
+                    +'<img class= "image" src="../'+cellData+'" alt="'
+                    +cellData+'"/></div></td>'));
+            } else if(cellData.split(",").length == 5){
+                hexcolours = convert_to_array(cellData);
+                var colourDiv = $('<td></td>');
+                for (i = 0; i < hexcolours.length; i++) { 
+                    colourDiv.append($('<div class="colorize" style="background-color:' +hexcolours[i]+'"></div>'));
+                }
+                row.append(colourDiv);
+            } else {
+                row.append($('<td>'+cellData+'</td>'));
+            }
+        });
+        table.append(row);
+    });
+    return table;
+}
+
+function tableHeaders(tableData) {
+    var table = $('<thead></thead>');
+
+    $(tableData).each(function (i, rowData) {
+        if(i == 0){
+            var row = $('<tr></tr>');
+
+            $(rowData).each(function (j, cellData) {
+                row.append($('<th>'+cellData+'</th>'));
+            });
+            table.append(row);
+        }
+    });
+    return table;
+}
+
+function convert_to_array(data){
+    var arr = data.replace("[", "");
+    arr = arr.replace("]", "");
+    arr = arr.replace(/'/g, '');
+    arr = arr.replace(/ /g, '');
+    hexcolours = arr.split(",");
+  
+    return hexcolours;
+}
+
+
+
+$(window).load(function(){
+    // Card View
+    var tables = $('.cards-table');
+    // Create an array containing all table headers
+    var table_headers = [];
+
+    tables.each(function () {
+        var th = [];
+        $(this).find('thead th').each(function () {
+            th.push($(this).text());
+        });
+        table_headers.push(th);
+    });
+    
+    // Add a data-label attribute to each cell with the value of the corresponding column header
+    // Iterate through each table
+    tables.each(function (table) {
+        var table_index = table;
+        // Iterate through each row
+        $(this).find('tbody tr').each(function () {
+            // Finally iterate through each column/cell
+            $(this).find('td').each(function (column) {
+                $(this).attr('data-label', table_headers[table_index][column]);
+            });
+        });
+    });
+})
+
+
---
+++ script.js
@@ -0,0 +1,102 @@
+
+
+function arrayToTable(tableData) {
+    var table = $('<table id="myTable"></table>');
+    $(tableData).each(function (i, rowData) {
+        var row = $('<tr height="210"></tr>');
+        var $placeholder = $(".holder");
+        $(rowData).each(function (j, cellData) {
+            if(cellData.includes('jpg')){
+                row.append($('<td><div class="holder">'
+                    +'<img src="../'+cellData+'" width="270" height="200" alt="'
+                    +cellData+'"/></div></td>'));
+            } else if(cellData.split(",").length == 5){
+                // TODO fix so that the colours are in the <td>
+                hexcolours = convert_to_array(cellData);
+                for (i = 0; i < hexcolours.length; i++) { 
+                    row.append($('<div class="colorize" style="background-color:' +hexcolours[i]+'"></div>'));
+                    console.log(hexcolours[i])
+                }
+            } else {
+                row.append($('<td><div class="holder">'+cellData+'</div></td>'));
+            }
+        });
+        table.append(row);
+    });
+    return table;
+}
+
+
+
+// function displaySwatch(hex) {
+//     console.log('display swatch');
+//     var $placeholder = $("#placeholder");
+//     var $tmp = $("<div class='colorize'></div>");
+//     $tmp.css("background-color", hex);
+//     // $tmp.append($("<span>").text(hex));
+//     $placeholder.add($tmp);
+//     // console.log($placeholder);
+// }
+
+// $(function() {
+//     for (i = 0; i < hexcolor.length; i++) { 
+//         displaySwatch(hexcolor[i])
+//     }
+// });
+
+$.ajax({
+    type: "GET",
+    url: "../test_metadata.csv",
+    success: function (data) {
+        $('body').append(arrayToTable(Papa.parse(data).data));
+    }
+});
+
+$(function() {
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable > tbody > tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+
+$(window).scroll(function(e){ 
+    var $el = $('.searchDiv'); 
+    var isPositionFixed = ($el.css('position') == 'fixed');
+    if ($(this).scrollTop() > 200 && !isPositionFixed){ 
+        $el.css({'position': 'fixed', 'top': '20px'}); 
+    }
+    if ($(this).scrollTop() < 200 && isPositionFixed){
+        $el.css({'position': 'static', 'top': '20px'}); 
+    } 
+});
+
+function convert_to_array(data){
+    var arr = data.replace("[", "");
+    arr = arr.replace("]", "");
+    arr = arr.replace(/'/g, '');
+    arr = arr.replace(/ /g, '');
+    hexcolours = arr.split(",");
+  
+    return hexcolours;
+}
+
+// function render_colours(colorData){
+//     var $placeholder = $("#placeholder");
+//     var $tmp;
+//     $.each(colorData, function(idx, elem) {
+//         $tmp = $("<div class='colorize'></div>");
+//         $tmp.css("background-color", "rgb(" + this[0] + "," + this[1] + "," + this[2] + ")");
+//         //$tmp.append($("<span>").text($tmp.css('background-color')));
+//         $tmp.append($("<span>").text("#" +
+//         twoDigitHex(this[0]) +
+//         twoDigitHex(this[1]) +
+//         twoDigitHex(this[2])));
+//         $placeholder.append($tmp);
+//   });
+// }
+
+// function twoDigitHex(val) {
+//     return ("00" + val.toString(16)).substr(-2).toUpperCase();
+//   }
